259fefa6b267b4d9eb19c936536ede34cfbfe67e
zwanner <owenvanderzwan@gmail.com>
2024-05-26 22:25:47 -0400

    more styling changes

--- Testing/client/src/components/Footer/index.jsx
+++ Testing/client/src/components/Footer/index.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
           </button>
         )}
         <h4>
-          GAMER SLOP
+          ~~~ GAMER SLOP ~~~
         </h4>
       </div>
     </footer>
--- Testing/client/src/components/Header/index.jsx
+++ Testing/client/src/components/Header/index.jsx
@@ -10,6 +10,7 @@ const Header = () => {
   };
   return (
     <header className="bg-light  mb-1 flex-row align-center ">
+      
       <div className="container flex-row justify-space-between-lg justify-center align-center">
 
         <div className="col">
