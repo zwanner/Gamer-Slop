86e1e59a56d58e1fc38d1ee4768dfd0ff07dbe23
Zwanner <50386890+zwanner@users.noreply.github.com>
2024-05-28 10:38:48 -0400

    Delete client directory

--- client/src/_mock/account.js
+++
@@ -1,7 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const account = {
-  displayName: 'Test User',
-  email: 'test@test.com',
-  photoURL: '/assets/images/avatars/avatar_18.jpg',
-};
--- client/src/sections/trough/view/index.js
+++
@@ -1,1 +0,0 @@
-export { default as TroughView } from './trough-view';
