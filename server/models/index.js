21fb60972fbd32e76c50fcc3d157898e099e57eb
Zwanner <50386890+zwanner@users.noreply.github.com>
2024-05-28 10:38:55 -0400

    Delete server directory

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-const Comment = require('./Comment');
-const Post = require('./Post');
-const Profile = require('./Profile');
-const User = require('./User');
-const Thought = require('./Thought');
-
-
-module.exports = { Comment, Post, Profile, User, Thought};
